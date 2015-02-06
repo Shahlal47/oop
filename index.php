195fd97f59993a58e4566cf9fecdca4f0ea98086
ahmedanjumfahad <f20_777@yahoo.com>
2015-02-06 11:34:49 +0600

    Working with basic object oriented programming

---
+++ index.php
@@ -0,0 +1,23 @@
+
+    <form action="index.php" method = "POST">
+        Id : <input type ="text" name ="id">
+        <br/>
+        Name : <input type ="text" name ="name">
+        <br/>
+        Salary : <input type ="text" name ="salary">
+        <br/>
+        <input type="submit" value ="Show Below" name ="showButton">
+        <br/>
+    </form>
+
+
+
+    <?php
+    if (isset($_POST["id"]) && isset($_POST["name"]) &&isset($_POST["salary"]))
+    {
+        $id = $_POST["id"];
+        $name = $_POST["name"];
+        $salary = $_POST["salary"];
+        echo "Id: ", $id, ", Name: ", $name, " , Salary: ", $salary;
+    }
+    ?>
